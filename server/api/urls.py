a83ffaff0fc11eb74a598213d6be77d655ceb989
Dark White <48309426+pysashapy@users.noreply.github.com>
2022-06-08 13:01:05 +0300

    Delete server directory

--- server/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import AuthView, UserView, ParkingView
-
-
-urlpatterns = [
-    path('auth', AuthView.as_view()),
-    path('user', UserView.as_view()),
-    path('parking', ParkingView.as_view()),
-]
