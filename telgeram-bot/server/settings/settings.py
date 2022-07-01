b4139f85fc06d7ab1199f79919db779b5cc0689f
pysashapy <sasha.2000ibr@gmail.com>
2022-07-02 00:43:27 +0300

    add exceptions

--- telgeram-bot/server/settings/settings.py
+++ telgeram-bot/server/settings/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-q!-kx53)5b2)^fdg59*rj!1xx_56sweet@6hjk3i2(ri2l)%3y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['192.168.1.58']
 
 # Application definition
 
