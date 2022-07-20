dce8c52b26bc562eb880af7c2e7ddd52e3f9cd19
pysashapy <sasha.2000ibr@gmail.com>
2022-07-20 18:49:59 +0300

    v 1.0

--- server/settings/settings.py
+++ server/settings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q!-kx53)5b2)^fdg59*rj!1xx_56sweet@6hjk3i2(ri2l)%3y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.1.58']
 
