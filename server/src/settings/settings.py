f18776a84e6776e245015a21d17c15b4e53681c0
pysashapy <sasha.2000ibr@gmail.com>
2022-07-21 22:55:42 +0300

    fixed static

--- server/src/settings/settings.py
+++ server/src/settings/settings.py
@@ -147,10 +147,8 @@ STATICFILES_FINDERS = (
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / "static",
-)
-STATIC_ROOT = BASE_DIR / 'static'
+
+STATIC_ROOT = BASE_DIR / 'static/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
