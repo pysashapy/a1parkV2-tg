11d8d0d7c26e4bcf0103eabe5b98f2b99f83d684
pysashapy <sasha.2000ibr@gmail.com>
2022-07-23 03:50:13 +0300

    fixed static

--- server/src/settings/urls.py
+++ server/src/settings/urls.py
@@ -23,5 +23,4 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
